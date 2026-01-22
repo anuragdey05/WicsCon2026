2caf0fc727ab7a45bfaa8586f6314f92ebfcfc3f
Anurag Dey <anurag@Anurags-MacBook-Air.local>
2026-01-22 20:30:14 +0530

    Setup GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/WicsCon2026/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
