7042e86dc577390302068fcc1716a06f94026a4d
Anurag Dey <anurag@Anurags-MacBook-Air.local>
2026-01-22 20:40:22 +0530

    bug fixing

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "/WicsCon2026",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
